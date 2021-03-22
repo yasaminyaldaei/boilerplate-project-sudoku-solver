2327ee16288cc082e22182acdabf279d051b2319
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2021-03-22 16:11:45 +0000

    add checkers

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,56 @@
+const LENGTH = 9;
+const INDEX_HELPER = new Array(LENGTH).fill(1);
+const REGION_HELPER = {
+  "[0,0]": 0,
+  "[0,1]": 1,
+  "[0,2]": 2,
+  "[1,0]": 9,
+  "[1,1]": 10,
+  "[1,2]": 11,
+  "[2,0]": 18,
+  "[2,1]": 19,
+  "[2,2]": 20,
+};
+
 class SudokuSolver {
 
   validate(puzzleString) {
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  rowIndexesGenerator(row) {
+    return INDEX_HELPER.map((_, index) => (LENGTH * row) + index)
+  }
 
+  checkRowPlacement(puzzleString, row, column, value) {
+    const rowIndex = row.charCodeAt(0) - 97;
+    const rowIndexes = this.rowIndexesGenerator(rowIndex);
+    return rowIndexes.findIndex(index => puzzleString.charAt(index) == value)
+  }
+
+  colIndexesGenerator(col) {
+    return INDEX_HELPER.map((_, index) => LENGTH * index + col);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const colIndexes = this.colIndexesGenerator(column);
+    return colIndexes.findIndex((index) => puzzleString.charAt(index) == value);
+  }
 
+  regionIndexesGenerator(row, col) {
+    const regionConstant = REGION_HELPER[`[${row / 3},${col / 3}]`];
+    return INDEX_HELPER.map(
+      (_, index) => (index % 3) + 3 * regionConstant + 9 * parseInt(index / 3)
+    );
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    const rowIndex = row.charCodeAt(0) - 97;
+    const regionIndexes = this.regionIndexesGenerator(rowIndex, column)
+    return regionIndexes.findIndex((index) => puzzleString.charAt(index) == value)
   }
 
   solve(puzzleString) {
-    
+
   }
 }
 
--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,33 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let solver = new SudokuSolver();
+  const notValidPuzzle = /[^\.\d]/
+  const notValidCoordinate = /[^A-I1-9]/
+  const notValidValue = /[^1-9]/
 
   app.route('/api/check')
     .post((req, res) => {
+      const puzzle = req.body.puzzle;
+      const coordinate = req.body.coordinate;
+      const value = req.body.value;
 
+      if (!puzzle || !coordinate || !value) {
+        return res.json({ "error": "Required field(s) missing" })
+      }
+      if (notValidPuzzle.test(puzzle)) return res.json({ "error": "Invalid characters in puzzle" })
+      if (puzzle.length !== 81) return res.json({ "error": "Expected puzzle to be 81 characters long" })
+      if (notValidCoordinate.test(coordinate)) return res.json({ "error": "Invalid coordinate" })
+      if (notValidValue.test(value)) return res.json({ "error": "Invalid value" })      
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
-
+      const puzzle = req.body.puzzle;
+      if (!puzzle) return res.json({ "error": "Required field missing" })
+      if (notValidPuzzle.test(puzzle)) return res.json({ "error": "Invalid characters in puzzle" })
+      if (puzzle.length !== 81) return res.json({ "error": "Expected puzzle to be 81 characters long" })
     });
 };
